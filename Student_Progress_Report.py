dbd1a094384dcea55e0bab48288385cfc6cbf3b4
Priyanshu Singh Rajawat <102310770+PRIYANSHUSINGH2003@users.noreply.github.com>
2023-04-19 11:01:14 +0530

    Update Student_Progress_Report.py

--- Student_Progress_Report.py
+++ Student_Progress_Report.py
@@ -5,31 +5,7 @@ from streamlit_pandas_profiling import st_profile_report
 from PIL import Image
 import numpy as np
 import plotly.figure_factory as ff
-import pyttsx3
-import datetime
 
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-# print(voices[1].id)
-engine.setProperty('voice', voices[0].id)
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
-
-def wishMe():
-    hour = int(datetime.datetime.now().hour)
-    if hour >= 0 and hour < 12:
-        speak("Good Morning!")
-
-    elif hour >= 12 and hour < 18:
-        speak("Good Afternoon!")
-
-    else:
-        speak("Good Evening!")
-
-    speak("Hello sir, How can I help you")
-
-wishMe()
 
 
 
