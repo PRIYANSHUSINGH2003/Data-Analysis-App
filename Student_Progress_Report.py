cebd78415ffa809ca5385464c7a62e24beb01977
Priyanshu Singh Rajawat <102310770+PRIYANSHUSINGH2003@users.noreply.github.com>
2023-09-15 15:55:16 +0530

    Update Student_Progress_Report.py

--- Student_Progress_Report.py
+++ Student_Progress_Report.py
@@ -9,9 +9,11 @@ import plotly.express as px
 import openpyxl
 
 import warnings
+import matplotlib  # Import matplotlib here
 
 # Filter out Matplotlib deprecation warnings
 warnings.filterwarnings("ignore", category=matplotlib.cbook.mplDeprecation)
+
 # Set the page title
 logo = Image.open("images/Logo.png")
 st.set_page_config(page_title='~AnalysisMaster',page_icon=logo,layout="wide")
